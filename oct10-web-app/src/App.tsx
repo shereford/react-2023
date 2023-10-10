c726904b3a476cd6cb77a1135f14ccfde4f54b40
Seth Hereford <shereford@circadianrisk.com>
2023-10-10 17:24:38 -0500

    update generic mantine libarary

--- oct10-web-app/src/App.tsx
+++ oct10-web-app/src/App.tsx
@@ -2,37 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "@mantine/core/styles.css";
 import "./App.css";
-import { Badge, Button, MantineProvider, createTheme } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  MantineProvider,
+  createTheme,
+  NumberInput,
+} from "@mantine/core";
 function randomNum() {}
-const theme = createTheme({
-  fontFamily: "Greycliff CF, sans-serif",
-  colors: {
-    "ocean-blue": [
-      "#7AD1DD",
-      "#5FCCDB",
-      "#44CADC",
-      "#2AC9DE",
-      "#1AC2D9",
-      "#11B7CD",
-      "#09ADC3",
-      "#0E99AC",
-      "#128797",
-      "#147885",
-    ],
-    "bright-pink": [
-      "#F0BBDD",
-      "#ED9BCF",
-      "#EC7CC3",
-      "#ED5DB8",
-      "#F13EAF",
-      "#F71FA7",
-      "#FF00A1",
-      "#E00890",
-      "#C50E82",
-      "#AD1374",
-    ],
-  },
-});
+const theme = createTheme({});
 function App() {
   //doesn't impact page look, unless you use it in the return statement
   // react variable, setRandomNum, is a function that takes in a number
@@ -43,18 +21,15 @@ function App() {
     <MantineProvider theme={theme}>
       <p>Generate a random number between</p>
       <div className="Seth">
-        <input
-          type="number"
+        <NumberInput
           value={lowNum}
-          onChange={(data) => setLowNum(Number(data.target.value))}
+          onChange={(data) => setLowNum(Number(data))}
         />
-        <input
-          type="number"
+        <NumberInput
           value={highNum}
-          onChange={(data) => setHighNum(data.target.value as any)}
+          onChange={(data) => setHighNum(data as any)}
         />
         <Button
-          color="ocean-blue"
           onClick={() => {
             setRandomNum(
               Math.floor(Math.random() * (highNum - lowNum)) + lowNum
